04778505627459a6b64a2893bd3091ccce42c6da
khuong123123123 <kukhe272025@gmail.com>
2025-06-15 23:08:46 +0700

    push code

---
+++ API/Models/Voucher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+    public class Voucher
+    {
+        [Key]
+        public int VoucherID { get; set; }
+
+        [StringLength(50)]
+        public string MaCode { get; set; } = string.Empty;
+
+        public float GiaTriGiam { get; set; }
+
+        public byte KieuGiamGia { get; set; }  // 0 = %, 1 = số tiền
+
+        public DateTime NgayBatDau { get; set; }
+
+        public DateTime NgayKetThuc { get; set; }
+
+        public int SoLuong { get; set; }
+
+    }
+}
