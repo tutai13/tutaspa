b8c57e36da353f9f4f9f8dcd533d96e776a148a2
duytuong279 <tuongpdpd09858@gmail.com>
2025-06-29 13:57:15 +0700

    qlysanpham

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -24,6 +24,9 @@
         <router-link to="/kho" class="menu-item" exact-active-class="active">
           <i class="fas fa-columns"></i> Quản lý kho
         </router-link>
+        <router-link to="/QuanLySanPham" class="menu-item" exact-active-class="active">
+          <i class="fas fa-columns"></i> Quản lý Sản Phẩm
+        </router-link>
         <router-link to="/employees" class="menu-item" exact-active-class="active">
           <i class="fas fa-columns"></i> Quản lý nhân viên
         </router-link>
